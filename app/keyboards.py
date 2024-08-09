115b4bfad36524adc094b3f70a532aa04b352e34
Maksim <knyazev.maksim2852@gmail.com>
2024-08-09 18:17:55 +0400

    add func get_inline_kb

--- app/keyboards.py
+++ app/keyboards.py
@@ -13,19 +13,12 @@ public_or_anonym: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
     input_field_placeholder='Нажми на кнопку...'
 )
 
-
-create_pool: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text='Создать опрос')]
-    ],
-    resize_keyboard=True,
-    one_time_keyboard=True,
-    input_field_placeholder='Нажми на кнопку...'
-)
-
-
-inline_kb: InlineKeyboardMarkup = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [InlineKeyboardButton(text='✅', callback_data='accept'), InlineKeyboardButton(text='❌', callback_data='cancel')]
-    ]
-)
+async def get_inline_kb(user_id: int) -> InlineKeyboardMarkup:
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(text='✅', callback_data=f'accept:{user_id}'),
+                InlineKeyboardButton(text='❌', callback_data=f'cancel:{user_id}')
+            ]
+        ]
+    )

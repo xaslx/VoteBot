4d46daf32f222d804217bf6ac39cb97eccf4591a
Maksim <knyazev.maksim2852@gmail.com>
2024-08-10 17:39:58 +0400

    del reply-keyboards

--- app/keyboards.py
+++ app/keyboards.py
@@ -3,22 +3,18 @@ from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
 
 
 
-public_or_anonym: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text='Анонимный')],
-        [KeyboardButton(text='Публичный')]
-    ],
-    resize_keyboard=True,
-    one_time_keyboard=True,
-    input_field_placeholder='Нажми на кнопку...'
-)
-
-async def get_inline_kb(user_id: int) -> InlineKeyboardMarkup:
+async def get_inline_kb(user_id: int, title: str, one_answer: str, two_answer: str) -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(
         inline_keyboard=[
             [
-                InlineKeyboardButton(text='✅', callback_data=f'accept:{user_id}'),
-                InlineKeyboardButton(text='❌', callback_data=f'cancel:{user_id}')
+                InlineKeyboardButton(
+                    text='✅', 
+                    callback_data=f'accept:{user_id}:{title}:{one_answer}:{two_answer}'
+                ),
+                InlineKeyboardButton(
+                    text='❌', 
+                    callback_data=f'cancel:{user_id}'
+                )
             ]
         ]
     )

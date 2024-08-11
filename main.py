959f910a8511326002f7c1f7153099b577275704
Maksim <knyazev.maksim2852@gmail.com>
2024-08-11 14:25:04 +0400

    add startup / shutdown

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ import logging
 from config import settings
 from app.handlers import router
 from aiogram.fsm.storage.redis import RedisStorage, Redis
+from database import connection
+from app.db_service import start_db
 
+
+
+async def start_bot():
+    await start_db()
+
+async def stop_bot():
+    await connection.close()
 
 
 async def main():
@@ -13,11 +22,13 @@ async def main():
     bot: Bot = Bot(token=settings.TOKEN_BOT, default=DefaultBotProperties(parse_mode='HTML'))
     redis: Redis = Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT)
     storage: RedisStorage = RedisStorage(redis=redis)
-
+    await bot.delete_webhook(True)
     dp: Dispatcher = Dispatcher(storage=storage)
-
     dp.include_router(router)
+    dp.startup.register(start_bot)
+    dp.shutdown.register(stop_bot)
     await dp.start_polling(bot)
+
 
 
 if __name__ == '__main__':

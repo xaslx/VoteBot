bb7ffb64ab30cc9edecc4a16a558beb5d4222780
Maksim <knyazev.maksim2852@gmail.com>
2024-08-11 14:25:26 +0400

    update keyboards

--- app/keyboards.py
+++ app/keyboards.py
@@ -3,17 +3,17 @@ from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
 
 
 
-async def get_inline_kb(user_id: int, title: str, one_answer: str, two_answer: str) -> InlineKeyboardMarkup:
+async def get_inline_kb(id_db: int) -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(
         inline_keyboard=[
             [
                 InlineKeyboardButton(
                     text='✅', 
-                    callback_data=f'accept:{user_id}:{title}:{one_answer}:{two_answer}'
+                    callback_data=f'accept:{id_db}'
                 ),
                 InlineKeyboardButton(
                     text='❌', 
-                    callback_data=f'cancel:{user_id}'
+                    callback_data=f'cancel:{id_db}'
                 )
             ]
         ]

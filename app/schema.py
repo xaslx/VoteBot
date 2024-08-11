5b46e175adc98c6b667d024ff753611300fef5ff
Maksim <knyazev.maksim2852@gmail.com>
2024-08-11 19:21:34 +0400

    update schema

--- app/schema.py
+++ app/schema.py
@@ -4,5 +4,4 @@ from aiogram.fsm.state import State, StatesGroup
 
 class Poll(StatesGroup):
     title: State = State()
-    one_answer: State = State()
-    two_answer: State = State()
+    answers: State = State()
